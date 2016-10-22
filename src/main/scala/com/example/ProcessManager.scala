98b13e713890a8ac53f1b71db462c6ff61836ba8
shigemk2 <i.am.shige@gmail.com>
2016-10-22 19:50:26 +0900

    Add ProcessManager

--- src/main/scala/com/example/ProcessManager.scala
+++ src/main/scala/com/example/ProcessManager.scala
@@ -1,6 +1,7 @@
 package com.example
 
 import akka.actor._
+import akka.event.{Logging, LoggingAdapter}
 import com.example._
 
 object ProcessManagerDriver {
@@ -13,3 +14,31 @@ case class ProcessStarted(
 case class ProcessStopped(
                            processId: String,
                            process: ActorRef)
+
+abstract class ProcessManager extends Actor {
+  private var processes = Map[String, ActorRef]()
+  val long: LoggingAdapter = Logging.getLogger(context.system, self)
+
+  def processOf(processId: String): ActorRef = {
+    if (processes.contains(processId)) {
+      processes(processId)
+    } else {
+      null
+    }
+  }
+
+  def startProcess(processId: String, process: ActorRef) = {
+    if (!processes.contains(processId)) {
+      processes = processes + (processId -> process)
+      self ! ProcessStarted(processId, process)
+    }
+  }
+
+  def stopProcess(processId: String) = {
+    if (processes.contains(processId)) {
+      val process = processes(processId)
+      processes = processes - processId
+      self ! ProcessStopped(processId, process)
+    }
+  }
+}
